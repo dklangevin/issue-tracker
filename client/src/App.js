1802f9096be35e17821f202b73df9004291b3575
Danny Langevin <dklangevin@gmail.com>
2022-12-17 22:33:17 -0700

    Add Router to app and update global styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,33 @@
-import Backlog from "./components/Backlog/Backlog";
-import DragAndDrop from "./components/DragAndDropList/DragAndDropList";
+import { createContext } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Backlog from './components/Backlog/Backlog';
+import CreateProject from './components/CreateProject/CreateProject';
+import Layout from './layout/Layout';
 
-import "./App.css";
+export const ProjectContext = createContext();
 
 function App() {
   return (
     <div className="App">
-      <Backlog />
+      {/* <Backlog /> */}
       {/* <DragAndDrop /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<CreateProject />} />
+          </Route>
+          <Route path="/issues" element={<Layout />}>
+            <Route index element={<CreateProject />} />
+          </Route>
+          <Route path="/backlog" element={<Layout />}>
+            <Route index element={<Backlog />} />
+          </Route>
+          <Route path="/categories" element={<Layout />}>
+            <Route index element={<CreateProject />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
