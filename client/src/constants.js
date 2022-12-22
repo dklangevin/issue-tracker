dc1a56b1b91397b526d26e98e4fa2e6a3756c2fc
Danny Langevin <dklangevin@gmail.com>
2022-12-21 20:59:48 -0700

    Update priorities constant

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,6 @@
-export const PRIORITIES = {
-  low: 'low',
-  medium: 'medium',
-  high: 'high',
-  critical: 'critical',
-};
+export const PRIORITIES = [
+  { name: 'low', color: 'rgb(139, 195, 74)' },
+  { name: 'medium', color: 'rgb(253, 216, 53)' },
+  { name: 'high', color: 'rgb(255, 167, 38)' },
+  { name: 'critical', color: 'rgb(239, 83, 80)' },
+];
