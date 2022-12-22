667d0c6708648fc3616e156d95a4b219df57612d
Danny Langevin <dklangevin@gmail.com>
2022-12-21 20:59:07 -0700

    Update ProjectDropdown component

--- client/src/components/ProjectDropdown/ProjectDropdown.js
+++ client/src/components/ProjectDropdown/ProjectDropdown.js
@@ -1,27 +1,40 @@
+import { Link } from 'react-router-dom';
+import useProject from '../../data/project';
 import useProjects from '../../data/projects';
 import useProjectContext from '../../hooks/projectContext';
-import { DownArrow } from '../../icons';
+import { DownArrow, Plus } from '../../icons';
 import styles from './ProjectDropdown.module.css';
 
 export default function ProjectDropdown() {
-  const { project, setProject } = useProjectContext();
+  const { project: projectId, setProject } = useProjectContext();
+  const project = useProject(projectId);
   const projects = useProjects();
+
+  const { name: projectName } = project || {};
 
   return (
     <div className={styles.container}>
-      <div className={styles.current}>
-        <span>{project}</span>
-        <DownArrow height={20} width={20} />
+      <Link to="/project" className={styles.current}>
+        <span>{projectName}</span>
+        <DownArrow className={styles.plus} />
+      </Link>
+      <div className={styles.wrapDropdown}>
+        <ul className={styles.dropdown}>
+          {projects
+            .filter(({ name }) => name !== projectName)
+            .map(({ id, name }, i) => (
+              <li key={i} onClick={() => setProject(id)}>
+                {name}
+              </li>
+            ))}
+          <hr />
+          <li>
+            <Link to="/project/create" className={styles.newProject}>
+              Create New Project <Plus className={styles.plus} />
+            </Link>
+          </li>
+        </ul>
       </div>
-      <ul className={styles.dropdown}>
-        {projects
-          .filter(({ title }) => title !== project)
-          .map(({ title }, i) => (
-            <li key={i} onClick={() => setProject(title)}>
-              {title}
-            </li>
-          ))}
-      </ul>
     </div>
   );
 }
