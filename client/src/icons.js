d0188b42dc86627558be3ded84a3db9fb89b5bc2
Danny Langevin <dklangevin@gmail.com>
2022-12-21 21:00:53 -0700

    Add Plus icon

--- client/src/icons.js
+++ client/src/icons.js
@@ -22,3 +22,9 @@ export const Close = (props) => (
     />
   </svg>
 );
+
+export const Plus = (props) => (
+  <svg viewBox="0 0 24 24" {...props}>
+    <path fill="currentColor" d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2Z" />
+  </svg>
+);
