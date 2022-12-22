c2b68cfa165e8e44ae1013a0389bedd32cf82ca1
Danny Langevin <dklangevin@gmail.com>
2022-12-21 21:01:01 -0700

    Update layout

--- client/src/layout/Layout.js
+++ client/src/layout/Layout.js
@@ -5,23 +5,33 @@ import { Logo } from '../icons';
 import styles from './Layout.module.css';
 import { useEffect, useState } from 'react';
 import useProjects from '../data/projects';
+import NavDropdown from '../components/NavDropdown/NavDropdown';
 
 const Layout = () => {
   const projects = useProjects();
   const [project, setProject] = useState();
-  useEffect(() => setProject(projects?.[0]?.title), [projects]);
+  useEffect(() => setProject(projects?.[0]?.id), [projects]);
   return (
     <ProjectContext.Provider value={{ project, setProject }}>
       <nav className={styles.layout}>
-        <Logo width={30} height={30} />
+        <Link to="/">
+          <Logo width={30} height={30} />
+        </Link>
         <ul>
-          <li>
+          {/* <li>
             <ProjectDropdown current="PROJECT-A" />
-          </li>
+          </li> */}
           <li>
-            <Link style={{ padding: 8 }} to="/issues">
+            {/* <Link style={{ padding: 8 }} to="/issues">
               Issues
-            </Link>
+            </Link> */}
+            <NavDropdown
+              item={{ name: 'Issues', link: '/issues' }}
+              items={[
+                { name: 'Active', link: '/issues' },
+                { name: 'Backlog', link: '/backlog' },
+              ]}
+            />
           </li>
           <li>
             <Link to="/backlog">Backlog</Link>
@@ -30,6 +40,10 @@ const Layout = () => {
             <Link to="/categories">Categories</Link>
           </li>
         </ul>
+        <ProjectDropdown
+          current="PROJECT-A"
+          className={styles.projectDropdown}
+        />
       </nav>
       <Outlet />
     </ProjectContext.Provider>
