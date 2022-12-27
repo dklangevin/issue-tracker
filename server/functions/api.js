7765fa956312b2df3f7edcf3e583bc38709aa41f
Danny Langevin <dklangevin@gmail.com>
2022-12-26 21:11:53 -0700

    Fix netlify files

---
+++ server/functions/api.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const routes = require('../route');
+
+const app = express();
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.use('/api', routes);
+
+app.get('/api', (req, res) => res.send('Hello from issue-tracker backend!'));
+
+module.exports.handler = serverless(app);
