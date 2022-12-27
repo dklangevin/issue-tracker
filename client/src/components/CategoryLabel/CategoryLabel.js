f8f8e6d37186ea4975228ad6b15eb081c19f1411
Danny Langevin <dklangevin@gmail.com>
2022-12-26 17:54:54 -0700

    Fix CategoryLabel component linting

--- client/src/components/CategoryLabel/CategoryLabel.js
+++ client/src/components/CategoryLabel/CategoryLabel.js
@@ -1,9 +1,9 @@
-import "./CategoryLabel.css";
-import classNames from "classnames";
+import './CategoryLabel.css';
+import classNames from 'classnames';
 
 function CategoryLabel(props) {
   return (
-    <div className={classNames("category-label", props.color)}>
+    <div className={classNames('category-label', props.color)}>
       {props.name}
     </div>
   );
