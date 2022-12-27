9df5a9f23bea152d1d8857339bb47ea5161aff10
Danny Langevin <dklangevin@gmail.com>
2022-12-26 17:48:38 -0700

    Update Avatar component

--- client/src/components/Avatar/Avatar.js
+++ client/src/components/Avatar/Avatar.js
@@ -1,8 +1,21 @@
-import "./Avatar.css";
-import portrait from "../../assets/img/portrait.png";
+import { Link } from 'react-router-dom';
+import useUser from '../../data/user';
+import styles from './Avatar.module.css';
 
 function Avatar(props) {
-  return <img className="avatar" alt="Assignee avatar" src={portrait}></img>;
+  // return (<img className={styles.container} alt="avatar" {...props} />);
+  const user = useUser(1);
+  const { id, first_name: first } = user || {};
+  return (
+    <Link to="/profile" className={styles.container}>
+      <img
+        src={`https://avatar.tobi.sh/${id}-${first}`}
+        alt="avatar"
+        {...props}
+      />
+      <span>DL</span>
+    </Link>
+  );
 }
 
 export default Avatar;
