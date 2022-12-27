85cf3d5b9e1bbfea338efbc1bd0b55343006c437
Danny Langevin <dklangevin@gmail.com>
2022-12-26 18:01:43 -0700

    Fix Project client route linting warnings

--- client/src/routes/Project/Project.js
+++ client/src/routes/Project/Project.js
@@ -1,61 +1,46 @@
-import useProject from '../../data/project';
-import useProjectUsers from '../../data/projects/users';
-import useProjectContext from '../../hooks/projectContext';
-import styles from './Project.module.css';
-import classNames from 'classnames';
-import Input from '../../components/Input/Input';
-import TextArea from '../../components/TextArea/TextArea';
-import Search from '../../components/Search/Search';
-import Button from '../../components/Button/Button';
-import { Close, Plus } from '../../icons';
-import { useRef } from 'react';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import { useMemo } from 'react';
-import useUsers from '../../data/users';
 import ProjectComponent from '../../components/Project/Project';
 
-export default function Project(props) {
-  const { project: id } = useProjectContext();
-  const project = useProject(id);
-  const projectUsers = useProjectUsers(id);
-  const [file, setFile] = useState();
-  const [preview, setPreview] = useState();
-  const [search, setSearch] = useState('');
-  const [addedUsers, setAddedUsers] = useState([]);
-  const ref = useRef();
-  const users = useUsers();
+export default function Project() {
+  // const { project: id } = useProjectContext();
+  // const project = useProject(id);
+  // const projectUsers = useProjectUsers(id);
+  // const [file, setFile] = useState();
+  // const [preview, setPreview] = useState();
+  // const [search, setSearch] = useState('');
+  // const [addedUsers, setAddedUsers] = useState([]);
+  // const ref = useRef();
+  // const users = useUsers();
 
-  const invites = [];
+  // const invites = [];
 
-  const { name, description } = project || {};
+  // const { name, description } = project || {};
 
-  useEffect(() => {
-    if (!file) {
-      setPreview();
-      return;
-    }
+  // useEffect(() => {
+  //   if (!file) {
+  //     setPreview();
+  //     return;
+  //   }
 
-    const objectUrl = URL.createObjectURL(file);
-    setPreview(objectUrl);
+  //   const objectUrl = URL.createObjectURL(file);
+  //   setPreview(objectUrl);
 
-    // free memory when ever this component is unmounted
-    return () => URL.revokeObjectURL(objectUrl);
-  }, [file]);
+  //   // free memory when ever this component is unmounted
+  //   return () => URL.revokeObjectURL(objectUrl);
+  // }, [file]);
 
-  const searchData = useMemo(
-    () =>
-      users
-        .filter(
-          (user) =>
-            !addedUsers.map((addedUser) => addedUser.id).includes(user.id)
-        )
-        .map((user) => ({
-          ...user,
-          value: `${user.first_name} ${user.last_name} - ${user.email}`,
-        })),
-    [users, addedUsers]
-  );
+  // const searchData = useMemo(
+  //   () =>
+  //     users
+  //       .filter(
+  //         (user) =>
+  //           !addedUsers.map((addedUser) => addedUser.id).includes(user.id)
+  //       )
+  //       .map((user) => ({
+  //         ...user,
+  //         value: `${user.first_name} ${user.last_name} - ${user.email}`,
+  //       })),
+  //   [users, addedUsers]
+  // );
 
   return <ProjectComponent />;
 
