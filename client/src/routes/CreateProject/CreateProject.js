252695749b0c1be032bcad8079fc1f06b937e428
Danny Langevin <dklangevin@gmail.com>
2022-12-26 17:51:55 -0700

    Update CreateProject route to use Project component

--- client/src/routes/CreateProject/CreateProject.js
+++ client/src/routes/CreateProject/CreateProject.js
@@ -8,6 +8,8 @@ import Button from '../../components/Button/Button';
 import Input from '../../components/Input/Input';
 import Search from '../../components/Search/Search';
 import styles from './CreateProject.module.css';
+import TextArea from '../../components/TextArea/TextArea';
+import ProjectComponent from '../../components/Project/Project';
 
 export default function CreateProject(props) {
   const users = useUsers();
@@ -15,7 +17,7 @@ export default function CreateProject(props) {
   const { setProject } = useProjectContext();
   const navigate = useNavigate();
 
-  const [name, setName] = useState('');
+  const [name, setName] = useState('New Project');
   const [description, setDescription] = useState('');
   const [search, setSearch] = useState('');
   const [addedUsers, setAddedUsers] = useState([]);
@@ -72,56 +74,84 @@ export default function CreateProject(props) {
     }
   };
 
-  return (
-    <div className={styles.container} {...props}>
-      <h3 className="title">Create New Project</h3>
-      <form onSubmit={handleSubmit}>
-        <Input
-          title="Name"
-          placeholder="Enter project name"
-          value={name}
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-          error={projectError}
-        />
-        <Input
-          title="Description"
-          placeholder="Enter project description"
-          value={description}
-          onChange={(e) => {
-            setDescription(e.target.value);
-          }}
-          error={descriptionError}
-        />
-        <Search
-          title="Invite Users"
-          data={searchData}
-          placeholder="Search by name or email"
-          value={search}
-          setValue={setSearch}
-          onChange={(e) => {
-            setSearch(e.target.value);
-          }}
-          onSelect={(val) => setAddedUsers((prev) => [...prev, val])}
-        />
-        <div className={styles.users}>
-          {addedUsers.map((user) => (
-            <span key={user.id} className={styles.user}>
-              {`${user.first_name} ${user.last_name}`}
-              <Close
-                className={styles.remove}
-                onClick={() =>
-                  setAddedUsers((prev) => prev.filter((u) => u.id !== user.id))
-                }
-              />
-            </span>
-          ))}
-        </div>
-        <Button type="submit" disabled={!isValid()} loading={loading}>
-          Create Project
-        </Button>
-      </form>
-    </div>
-  );
+  return <ProjectComponent create />;
+
+  // return (
+  //   <div className={styles.container} {...props}>
+  //     {/* <h3 className="title">Create New Project</h3> */}
+  //     <Input
+  //       placeholder="Enter project name"
+  //       value={name}
+  //       onChange={(e) => {
+  //         setName(e.target.value);
+  //       }}
+  //       error={projectError}
+  //       className={styles.title}
+  //     />
+  //     <div className={styles.details}>
+  //       <div className={styles.wrapImage}>
+  //         <img
+  //           className={styles.image}
+  //           src={'https://avatar.tobi.sh/abcdegh'}
+  //         />
+  //         <span>Edit Image</span>
+  //       </div>
+  //       <form onSubmit={handleSubmit}>
+  //         {/* <Input
+  //             placeholder="Enter project name"
+  //             value={name}
+  //             onChange={(e) => {
+  //               setName(e.target.value);
+  //             }}
+  //             error={projectError}
+  //             className={styles.title}
+  //           /> */}
+  //         {/* <Input
+  //               placeholder="Enter project description"
+  //               value={description}
+  //               onChange={(e) => {
+  //                 setDescription(e.target.value);
+  //               }}
+  //               error={descriptionError}
+  //               className={styles.description}
+  //             /> */}
+  //         {/* <textarea
+  //             placeholder="Enter project description"
+  //             className={styles.description}
+  //           /> */}
+  //         <TextArea placeholder="Enter project description" />
+
+  //         <Search
+  //           title="Invite Users"
+  //           data={searchData}
+  //           placeholder="Search by name or email"
+  //           value={search}
+  //           setValue={setSearch}
+  //           onChange={(e) => {
+  //             setSearch(e.target.value);
+  //           }}
+  //           onSelect={(val) => setAddedUsers((prev) => [...prev, val])}
+  //         />
+  //         <div className={styles.users}>
+  //           {addedUsers.map((user) => (
+  //             <span key={user.id} className={styles.user}>
+  //               {`${user.first_name} ${user.last_name}`}
+  //               <Close
+  //                 className={styles.remove}
+  //                 onClick={() =>
+  //                   setAddedUsers((prev) =>
+  //                     prev.filter((u) => u.id !== user.id)
+  //                   )
+  //                 }
+  //               />
+  //             </span>
+  //           ))}
+  //         </div>
+  //         <Button type="submit" disabled={!isValid()} loading={loading}>
+  //           Create Project
+  //         </Button>
+  //       </form>
+  //     </div>
+  //   </div>
+  // );
 }
