10891d9db685bcc6cabce827810e6a0b25d1f3d4
Danny Langevin <dklangevin@gmail.com>
2022-12-26 17:53:17 -0700

    Update logos

--- client/src/icons.js
+++ client/src/icons.js
@@ -1,7 +1,102 @@
 export const Logo = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    id="eUxmCwETnbo1"
+    viewBox="0 0 203 56"
+    shapeRendering="geometricPrecision"
+    textRendering="geometricPrecision"
+    {...props}
+  >
+    <rect
+      width={182}
+      height={40.766}
+      rx={4}
+      ry={4}
+      transform="translate(15.164 7.617) skewX(-14)"
+      fill="#22aaa1"
+      strokeWidth={0}
+    />
+    <rect
+      width={10}
+      height={29.109}
+      rx={3}
+      ry={3}
+      transform="translate(22.258 13.446) skewX(-14)"
+      fill="#fff"
+      strokeWidth={0}
+    />
+    <rect
+      width={15}
+      height={29.109}
+      rx={3}
+      ry={3}
+      transform="translate(37.258 13.446) skewX(-14)"
+      fill="rgba(255,255,255,0.85)"
+      strokeWidth={0}
+    />
+    <rect
+      width={20}
+      height={29.109}
+      rx={3}
+      ry={3}
+      transform="translate(57.258 13.446) skewX(-14)"
+      fill="rgba(255,255,255,0.7)"
+      strokeWidth={0}
+    />
+    <text
+      dx={0}
+      dy={0}
+      // fontFamily='"eUxmCwETnbo1:::Manrope"'
+      fontFamily="Manrope, sans-serif"
+      fontSize={36}
+      fontWeight={700}
+      fontStyle="italic"
+      transform="translate(76.353 40.77)"
+      fill="#fff"
+      strokeWidth={0}
+    >
+      trackr
+      <tspan y={0} />
+      <tspan x={0} y={36} />
+    </text>
+  </svg>
+);
+
+export const Logo1 = (props) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" {...props}>
     <rect width={100} height={100} rx={20} fill="#22aaa1" />
     <path fill="#fff" d="M41.09 81.5v-63h17.82v63H41.09Z" />
+  </svg>
+);
+
+export const Logo2 = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 62 56"
+    shapeRendering="geometricPrecision"
+    textRendering="geometricPrecision"
+    {...props}
+  >
+    <rect
+      width={43.871}
+      height={43.871}
+      rx={4}
+      ry={4}
+      transform="translate(9.236 2) skewX(-10)"
+      fill="#fff"
+      stroke="#fff"
+      strokeWidth={4}
+    />
+    <rect
+      width={43.871}
+      height={43.871}
+      rx={4}
+      ry={4}
+      transform="translate(16.236 10) skewX(-10)"
+      fill="#22aaa1"
+      stroke="#22aaa1"
+      strokeWidth={4}
+    />
   </svg>
 );
 
