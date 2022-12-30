d93ee9fca2d22ba7aff34abe33dd56dc48da5a36
Danny Langevin <dklangevin@gmail.com>
2022-12-30 16:19:45 -0700

    Add Nav component

---
+++ client/src/Nav/Nav.js
@@ -0,0 +1,47 @@
+import { Link } from 'react-router-dom';
+import Avatar from '../components/Avatar/Avatar';
+import NavDropdown from '../components/NavDropdown/NavDropdown';
+import ProjectDropdown from '../components/ProjectDropdown/ProjectDropdown';
+// import { Logo } from '../icons';
+import Button from '../components/Button/Button';
+import useProjects from '../data/projects';
+import { Plus } from '../icons';
+import { Logo2 } from '../logo';
+import styles from './Nav.module.css';
+
+export default function Nav() {
+  const projects = useProjects();
+
+  return (
+    <nav className={styles.container}>
+      <Link to="/" className={styles.logo}>
+        <Logo2 />
+      </Link>
+      <ul>
+        <li>
+          <NavDropdown
+            item={{ name: 'Issues', link: '/issues' }}
+            items={[
+              { name: 'Active', link: '/issues' },
+              { name: 'Backlog', link: '/backlog' },
+            ]}
+          />
+        </li>
+        <li>
+          <Link to="/categories">Categories</Link>
+        </li>
+      </ul>
+      {projects.length ? (
+        <ProjectDropdown
+          current="PROJECT-A"
+          className={styles.projectDropdown}
+        />
+      ) : (
+        <Button to="/project/create" className={styles.createNewProject}>
+          Create New Project <Plus className={styles.plus} />
+        </Button>
+      )}
+      <Avatar size={48} />
+    </nav>
+  );
+}
