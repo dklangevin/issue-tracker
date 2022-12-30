17aa633c8132943f34b7bc8ba9bd99cd5513f931
Danny Langevin <dklangevin@gmail.com>
2022-12-30 16:22:08 -0700

    Update Button component

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -1,13 +1,40 @@
 import styles from './Button.module.css';
 import classNames from 'classnames';
+import { Link } from 'react-router-dom';
 
-function Button({ children, appearance, loading, ...props }) {
-  return (
+function Button({ children, to, appearance, loading, className, ...props }) {
+  return to ? (
+    <Link
+      to={to}
+      className={classNames(styles.link, ...(className ? [className] : []))}
+    >
+      <button
+        className={classNames(
+          styles.container,
+          appearance === 'secondary'
+            ? styles.secondary
+            : appearance === 'tertiary'
+            ? styles.tertiary
+            : styles.primary,
+          ...(loading ? [styles.loading] : []),
+          ...(className ? [className] : [])
+        )}
+        {...props}
+      >
+        {children}
+      </button>
+    </Link>
+  ) : (
     <button
       className={classNames(
-        styles.button,
-        appearance === 'secondary' ? styles.secondary : styles.primary,
-        ...(loading ? [styles.loading] : [])
+        styles.container,
+        appearance === 'secondary'
+          ? styles.secondary
+          : appearance === 'tertiary'
+          ? styles.tertiary
+          : styles.primary,
+        ...(loading ? [styles.loading] : []),
+        ...(className ? [className] : [])
       )}
       {...props}
     >
