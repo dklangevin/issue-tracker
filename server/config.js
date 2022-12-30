d126bbf9064e19027f2fa441b92ce27c98922ea3
Danny Langevin <dklangevin@gmail.com>
2022-12-30 16:29:04 -0700

    Add server config file

---
+++ server/config.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const config = {
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+};
+
+module.exports = config;
