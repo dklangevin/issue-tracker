5f0d4ccb4d553ceb34863676ed4496b201ad4f5a
Danny Langevin <dklangevin@gmail.com>
2022-12-30 16:20:51 -0700

    Clean up layout

--- client/src/layout/Layout.js
+++ client/src/layout/Layout.js
@@ -1,12 +1,10 @@
-import { Link, Outlet } from 'react-router-dom';
-import ProjectDropdown from '../components/ProjectDropdown/ProjectDropdown';
-import { ProjectContext } from '../hooks/projectContext';
-import { Logo } from '../icons';
-import styles from './Layout.module.css';
 import { useEffect, useState } from 'react';
+import { Outlet } from 'react-router-dom';
 import useProjects from '../data/projects';
-import NavDropdown from '../components/NavDropdown/NavDropdown';
-import Avatar from '../components/Avatar/Avatar';
+import { ProjectContext } from '../hooks/projectContext';
+import Nav from '../Nav/Nav';
+import styles from './Layout.module.css';
+import MobileNav from '../MobileNav/MobileNav';
 
 const Layout = () => {
   const projects = useProjects();
@@ -14,37 +12,12 @@ const Layout = () => {
   useEffect(() => setProject(projects?.[0]?.id), [projects]);
   return (
     <ProjectContext.Provider value={{ project, setProject }}>
-      <nav className={styles.layout}>
-        <Link to="/" className={styles.logo}>
-          <Logo />
-        </Link>
-        <ul>
-          <li>
-            <NavDropdown
-              item={{ name: 'Issues', link: '/issues' }}
-              items={[
-                { name: 'Active', link: '/issues' },
-                { name: 'Backlog', link: '/backlog' },
-              ]}
-            />
-          </li>
-          <li>
-            <Link to="/backlog">Backlog</Link>
-          </li>
-          <li>
-            <Link to="/categories">Categories</Link>
-          </li>
-        </ul>
-        <ProjectDropdown
-          current="PROJECT-A"
-          className={styles.projectDropdown}
-        />
-        <Avatar />
-      </nav>
-      <Outlet />
+      <Nav />
+      <MobileNav />
+      <div className={styles.content}>
+        <Outlet />
+      </div>
     </ProjectContext.Provider>
-
-    // </ProjectContext.Provider>
   );
 };
 
