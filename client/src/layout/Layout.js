83c4713b3afdbd640ac47dbf1deb113a935ed7e0
Danny Langevin <dklangevin@gmail.com>
2022-12-31 17:06:14 -0700

    Add sidebar to layout

--- client/src/layout/Layout.js
+++ client/src/layout/Layout.js
@@ -5,17 +5,47 @@ import { ProjectContext } from '../hooks/projectContext';
 import Nav from '../Nav/Nav';
 import styles from './Layout.module.css';
 import MobileNav from '../MobileNav/MobileNav';
+import { Link } from 'react-router-dom';
+import { CategoriesIcon, IssuesIcon, LeftArrowIcon, TeamIcon } from '../icons';
 
 const Layout = () => {
   const projects = useProjects();
   const [project, setProject] = useState();
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   useEffect(() => setProject(projects?.[0]?.id), [projects]);
+
   return (
     <ProjectContext.Provider value={{ project, setProject }}>
-      <Nav />
-      <MobileNav />
-      <div className={styles.content}>
-        <Outlet />
+      <div className={styles.container}>
+        <div>
+          {sidebarOpen && (
+            <div className={styles.sidebar}>
+              <LeftArrowIcon onClick={() => setSidebarOpen(false)} />
+              <ul>
+                <li>
+                  <IssuesIcon />
+                  <Link to="/issues">Issues</Link>
+                </li>
+                <li>
+                  <CategoriesIcon />
+                  <Link to="/categories">Categories</Link>
+                </li>
+                <li>
+                  <TeamIcon />
+                  <Link to="/team">Team</Link>
+                </li>
+              </ul>
+            </div>
+          )}
+          <div className={sidebarOpen ? styles.blur : null}>
+            <Nav />
+            <MobileNav setSidebarOpen={setSidebarOpen} />
+            <div className={styles.content}>
+              <Outlet />
+            </div>
+          </div>
+        </div>
       </div>
     </ProjectContext.Provider>
   );
