daca2d28a2f33dca7385ffe3453a95039dd654c3
Danny Langevin <dklangevin@gmail.com>
2023-01-01 23:33:08 -0700

    Add BottomNav component

---
+++ client/src/BottomNav/BottomNav.js
@@ -0,0 +1,37 @@
+import { Link } from 'react-router-dom';
+import { IssuesIcon, CategoriesIcon, TeamIcon } from '../icons';
+import styles from './BottomNav.module.css';
+import { useBottomNavHidden } from '../hooks/scrollPosition';
+
+export default function BottomNav(props) {
+  const hidden = useBottomNavHidden();
+
+  return (
+    <ul
+      className={styles.container}
+      style={{
+        bottom: hidden ? -72 : 0,
+      }}
+      {...props}
+    >
+      <li>
+        <Link to="/issues">
+          <IssuesIcon />
+          Issues
+        </Link>
+      </li>
+      <li>
+        <Link to="/categories">
+          <CategoriesIcon />
+          Categories
+        </Link>
+      </li>
+      <li>
+        <Link to="/team">
+          <TeamIcon />
+          Team
+        </Link>
+      </li>
+    </ul>
+  );
+}
