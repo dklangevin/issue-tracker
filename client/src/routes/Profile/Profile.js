635be94b1ec7844dd731dc34b40adb6793ebf152
Danny Langevin <dklangevin@gmail.com>
2023-01-01 23:35:41 -0700

    Update Profile client route

--- client/src/routes/Profile/Profile.js
+++ client/src/routes/Profile/Profile.js
@@ -5,15 +5,25 @@ import useProjects from '../../data/projects';
 import useUser from '../../data/user';
 import { generateLinearGradientBackground } from '../../util';
 import styles from './Profile.module.css';
+import { Link } from 'react-router-dom';
+import useProjectContext from '../../hooks/projectContext';
+import { useEffect } from 'react';
 
 export default function Profile(props) {
   const [modalHidden, setModalHidden] = useState(true);
   const projects = useProjects();
-  const user = useUser(1);
+  const _user = useUser(1);
+  const [user, setUser] = useState({});
+
+  const { setProject } = useProjectContext();
 
   const { id, first_name: first, last_name: last, avatar } = user || {};
 
   const initials = first && last ? `${first[0]}${last[0]}` : '';
+
+  useEffect(() => {
+    setUser(_user);
+  }, [_user]);
 
   return (
     <div className={styles.container} {...props}>
@@ -56,7 +66,7 @@ export default function Profile(props) {
             placeholder="Enter project name"
             value={`${first} ${last}`}
             onChange={() => {
-              // setName(e.target.value);
+              // setUser((prev) => ({ ...prev, first_name: e.target.value }));
             }}
             // error={projectError}
             className={styles.title}
@@ -73,8 +83,8 @@ export default function Profile(props) {
           <ul className={styles.users}>
             {projects.length ? (
               projects.map(({ id, name }) => (
-                <li key={id}>
-                  <span>{name}</span>
+                <li key={id} onClick={() => setProject(id)}>
+                  <Link to="/project">{name}</Link>
                 </li>
               ))
             ) : (
