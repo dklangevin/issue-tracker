52101fa506f5750bc6ed8d5815dc529faf2b1fcc
Danny Langevin <dklangevin@gmail.com>
2023-02-19 12:25:02 -0700

    Add test api

--- server/route/index.js
+++ server/route/index.js
@@ -2,11 +2,14 @@ const { Router } = require('express');
 const projects = require('./projects');
 const users = require('./users');
 const issues = require('./issues');
+const { getTeams } = require('./test');
 
 const router = new Router();
 
 router.use('/projects', projects);
 router.use('/users', users);
 router.use('/issues', issues);
+
+router.get('/test', getTeams);
 
 module.exports = router;
---
+++ server/route/test.js
@@ -0,0 +1,57 @@
+const axios = require('axios');
+
+const api = axios.create({
+  baseURL: 'https://stats.nba.com/stats/',
+  timeout: 1000,
+  headers: {
+    Referer: 'https://www.nba.com/',
+    Origin: 'https://www.nba.com',
+    Accept: '*/*',
+    // Host: 'stats.nba.com',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Access-Control-Allow-Origin': '*',
+    // Connection: 'keep-alive',
+  },
+});
+
+async function listTeams() {
+  console.log('fetching teams');
+  const options = {
+    method: 'GET',
+    headers: {
+      Referer: 'https://www.nba.com',
+      // Origin: 'https://www.nba.com',
+      'Referrer-Policy': 'no-referrer',
+      Accept: '*/*',
+    },
+  };
+  const rows = await api
+    .get(
+      'leaguestandingsv3?GroupBy=conf&LeagueID=00&Season=2022-23&SeasonType=Regular%20Season&Section=overall'
+    )
+    .then((res) => {
+      const data = res.data.resultSets[0].rowSet;
+      const rows = data.map((row) => ({ id: row[2], name: row[4] }));
+      return rows;
+    })
+    .catch((err) => {
+      console.log(err);
+      console.log(err.request);
+    });
+  console.log('teams fetch success');
+  return rows;
+}
+
+const getTeams = async (req, res) => {
+  try {
+    const rows = await listTeams();
+    res.json(rows);
+    // res.json('test');
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+module.exports = {
+  getTeams,
+};
